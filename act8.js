4e8b603bb5c9e7bac318bde6b9cf3dbfa62e372d
marcchesterbatin <batinchester.business@gmail.com>
2023-10-13 10:46:24 +0800

    feat: added javascript into my html

---
+++ act8.js
@@ -0,0 +1,20 @@
+//activity 8 
+// ARRAYS
+
+const favSingers =[ 'Bruno Mars', ' Adelle', ' Dua Lipa' ];
+
+console.log ( favSingers [0]);
+console.log ( favSingers [1]);
+console.log ( favSingers [2]);
+
+
+const favNumbers = [ '7', '9', '27', '28'];
+
+console.log ( favNumbers [0]);
+console.log ( favNumbers [1]);
+console.log ( favNumbers [2]);
+console.log ( favNumbers [3]);
+
+const mixedArr = [ " string ", ["otherarray"], 123, true];
+
+console.log ( favSingers [0], favSingers [1], favSingers[2], favNumbers [1], mixedArr [3]);
