8722df51bf2738000032368aa3a6da8147cca652
marcchesterbatin <batinchester.business@gmail.com>
2023-10-19 15:28:59 +0800

    feat: added javascript into my html

---
+++ act9.js
@@ -0,0 +1,37 @@
+// concat
+
+const arr1 = [ " Cecile ", " Lone "];
+const arr2 = [ " Emil ", " Tobias ", " Linus"];
+
+const children = arr1.concat(arr2);
+console.log( children);
+
+// includes
+
+let = " Hello world, welcome to the universe."
+let result = text.include("world");
+console.log (result);
+
+// push
+
+const fruits = [" Banana ", " Orange ", " Apple", " Mango"];
+fruits.push (" Kiwi");
+console.log (fruits);
+
+// unshift
+
+const array1 = [" 1 ", "2 ", " 3 "];
+console.log ( array1.unshift(4,5));
+console.log(array1);
+
+//pop
+
+fruits.pop();
+console.log(fruits);
+
+// shift
+
+const array2 = [" 1 ", " 2 ", " 3 "];
+const firstElement = array2.shift();
+console.log(array2);
+console.log( firstElement);
